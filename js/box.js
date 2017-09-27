e063814996067658ddb1bd46c0037c76cf222d96
thefellowes <marcogeertsma@gmail.com>
2017-09-27 13:46:11 +0200

    Visual changes
    Improved the table (added height to it), cleaned up box.js,
    started adding textures to support walls and floors so lighting is better to see.

--- js/box.js
+++ js/box.js
@@ -2,14 +2,13 @@
 class box {
     constructor(x, y, z, constructorColor, height, width, depth){
         var boxMaterial = new THREE.MeshPhongMaterial({
-	    color : constructorColor
-    });			
+			color : constructorColor
+		});			
 
-    var boxSize = new THREE.BoxGeometry(height,width,depth);
-    var box = new THREE.Mesh(boxSize, boxMaterial);
+		var boxSize = new THREE.BoxGeometry(height,width,depth);
+		var box = new THREE.Mesh(boxSize, boxMaterial);
 
-    box.position.set(x, y, z);
-    return box;
+		box.position.set(x, y, z);
+		return box;
     }
-
 }
--- js/main.js
+++ js/main.js
@@ -1,86 +1,98 @@
-var scene = new THREE.Scene();
-var aspect = window.innerWidth / window.innerHeight;
-var camera = new THREE.PerspectiveCamera( 75, aspect, 0.1, 1000 );
-var renderer = new THREE.WebGLRenderer({antialias:true});
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+	var scene = new THREE.Scene();
+	var aspect = window.innerWidth / window.innerHeight;
+	var camera = new THREE.PerspectiveCamera( 75, aspect, 0.1, 1000 );
+	var renderer = new THREE.WebGLRenderer({antialias:true});
+	renderer.setSize( window.innerWidth, window.innerHeight );
+	document.body.appendChild( renderer.domElement );
 
-console.log(renderer);
+	console.log(renderer);
 
-var controls = new THREE.OrbitControls(camera, renderer.domElement);
-controls.target.set(0,0,0);
-//controls.update();
+	var controls = new THREE.OrbitControls(camera, renderer.domElement);
+	controls.target.set(0,0,0);
+	//controls.update();
 
-//TextureLoader
-var loader = new THREE.TextureLoader();
-var texture = loader.load("img/green_mat.png");
+	//TextureLoader
+	var loader = new THREE.TextureLoader();
+	var texture = loader.load("img/green_mat.png");
 
-// x, y, z, color, width, height, depth
-var green_mat = new box(0, 0, 0, '#008000',2.8 , 0.1, 1.6);
-var wall_1 = new box(1.4, 0, 0, '#835C3B', 0.1, 0.2, 1.7);
-var wall_2 = new box(-1.4, 0, 0, '#835C3B', 0.1, 0.2, 1.7);
-var wall_3 = new box(0, 0, 0.8, '#835C3B', 2.7, 0.2, 0.1);
-var wall_4 = new box(0, 0, -0.8, '#835C3B', 2.7, 0.2, 0.1);
+	//Room Textures
+	var floor = new box(0, 2, 0, 'floorTexture', 2.8, 0.1, 1.6);
+	
+	//Table
+	// x, y, z, color, width, height, depth
+	var green_mat = new box(0, 1, 0, texture,2.8 , 0.1, 1.6);
+	var wall_1 = new box(1.4, 1, 0, '#835C3B', 0.1, 0.2, 1.7);
+	var wall_2 = new box(-1.4, 1, 0, '#835C3B', 0.1, 0.2, 1.7);
+	var wall_3 = new box(0, 1, 0.8, '#835C3B', 2.7, 0.2, 0.1);
+	var wall_4 = new box(0, 1, -0.8, '#835C3B', 2.7, 0.2, 0.1);
+	var leg_1 = new box(1.4, 0.5, -0.8, '#835C3B', 0.1, 1, 0.1);
+	var leg_2 = new box(1.4, 0.5, 0.8, '#835C3B', 0.1, 1, 0.1);
+	var leg_3 = new box(-1.4, 0.5, 0.8, '#835C3B', 0.1, 1, 0.1);
+	var leg_4 = new box(-1.4, 0.5, -0.8, '#835C3B', 0.1, 1, 0.1);
 
-var eight_ball_table = new THREE.Group();
-eight_ball_table.add(green_mat);
-eight_ball_table.add(wall_1);
-eight_ball_table.add(wall_2);
-eight_ball_table.add(wall_3);
-eight_ball_table.add(wall_4);
-scene.add(eight_ball_table);
+	var eight_ball_table = new THREE.Group();
+	eight_ball_table.add(green_mat);
+	eight_ball_table.add(wall_1);
+	eight_ball_table.add(wall_2);
+	eight_ball_table.add(wall_3);
+	eight_ball_table.add(wall_4);
+	eight_ball_table.add(leg_1);
+	eight_ball_table.add(leg_2);
+	eight_ball_table.add(leg_3);
+	eight_ball_table.add(leg_4);
+	scene.add(eight_ball_table);
 
-//Creating the balls (lol)
-var white_ball = new ball(-1.0, 0.1, 0, '#FFFFFF');
-var ball_1 = new ball(1.0, 0.1, 0, '#FF0000');
-var ball_2 = new ball(1.1, 0.1, 0, '#FFFF00');
-var ball_3 = new ball(1.2, 0.1, 0, '#008000');
-var ball_4 = new ball(1.3, 0.1, 0, '#FF69B4');
+	//Creating the balls (lol)
+	var white_ball = new ball(-1.0, 1.1, 0, '#FFFFFF');
+	var ball_1 = new ball(1.0, 1.1, 0, '#FF0000');
+	var ball_2 = new ball(1.1, 1.1, 0, '#FFFF00');
+	var ball_3 = new ball(1.2, 1.1, 0, '#008000');
+	var ball_4 = new ball(1.3, 1.1, 0, '#FF69B4');
 
-scene.add(white_ball);
-scene.add(ball_1); 
-scene.add(ball_2); 
-scene.add(ball_3); 
-scene.add(ball_4);
+	scene.add(white_ball);
+	scene.add(ball_1); 
+	scene.add(ball_2); 
+	scene.add(ball_3); 
+	scene.add(ball_4);
 
-//Set the camera position
-camera.position.x = -2;
-camera.position.y = 1.5;
-camera.position.z = 1.5;
-camera.lookAt(green_mat.position);
+	//Set the camera position
+	camera.position.x = -2;
+	camera.position.y = 1.5;
+	camera.position.z = 1.5;
+	camera.lookAt(green_mat.position);
 
-// // create a point light
-const pointLight = new THREE.PointLight(0xFFFFFF);
-// set the position of the light
-pointLight.position.x = 0;
-pointLight.position.y = 20;
-pointLight.position.z = 0;
-pointLight.lookAt( green_mat );
-scene.add(pointLight);
-var Light = new THREE.DirectionalLight( 0xdddddd, 0.8 );
-Light.position.set( -8, 8, 8 );
-Light.lookAt ( green_mat );
-scene.add(Light);
+	// // create a point light
+	const pointLight = new THREE.PointLight(0xFFFFFF);
+	// set the position of the light
+	pointLight.position.x = 0;
+	pointLight.position.y = 20;
+	pointLight.position.z = 0;
+	pointLight.lookAt( green_mat );
+	scene.add(pointLight);
+	var Light = new THREE.DirectionalLight( 0xdddddd, 0.8 );
+	Light.position.set( -8, 8, 8 );
+	Light.lookAt ( green_mat );
+	scene.add(Light);
 
-var LightA = new THREE.AmbientLight( 0x000 );
-scene.add(LightA);
-//light.position.set ()
-var grid = new THREE.GridHelper(100, 100);
-scene.add(grid);
-var axisHelper = new THREE.AxisHelper( 20 );
-scene.add( axisHelper );
+	var LightA = new THREE.AmbientLight( 0x000 );
+	scene.add(LightA);
+	//light.position.set ()
+	var grid = new THREE.GridHelper(100, 100);
+	scene.add(grid);
+	var axisHelper = new THREE.AxisHelper( 20 );
+	scene.add( axisHelper );
 
-  // add to the scene
-var render = function () {
-    renderer.render( scene, camera );
-    requestAnimationFrame( render );
-    controls.update();
-};
+	  // add to the scene
+	var render = function () {
+		renderer.render( scene, camera );
+		requestAnimationFrame( render );
+		controls.update();
+	};
 
-function animate() {
-   requestAnimationFrame( animate );
-   //controls.update();
-}
+	function animate() {
+	   requestAnimationFrame( animate );
+	   //controls.update();
+	}
 
-animate();
-render();
+	animate();
+	render();
